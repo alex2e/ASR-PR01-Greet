5b4384c824b88c2a2ae3e43faf0da9ebf71db4ae
alex2e <a.santillana@aol.com>
2018-09-24 20:24:34 +0200

    V.2

--- app/src/main/java/com/example/santi/asr_pr01_greet/MainActivity.kt
+++ app/src/main/java/com/example/santi/asr_pr01_greet/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.santi.asr_pr01_greet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clicks()
     }
+
+    private fun clicks() {
+        btnGreet.setOnClickListener {
+            if (txtName.text.isEmpty() && txtSurname.text.isEmpty()){
+                Toast.makeText(this,getString(R.string.you_must_fill_the_fields), Toast.LENGTH_SHORT).show()
+            }else{
+                when(chkPolite.isChecked){
+                    false -> {
+                        lblGreet.text = String.format("Welcome %s %s", txtName.text, txtSurname.text)
+                    }
+                    true -> {
+                        lblGreet.text = String.format("Welcome Mr/Ms %s %s",  txtName.text, txtSurname.text)
+                    }
+                }
+            }
+        }
+
+        btnReset.setOnClickListener {
+            txtName.text = null
+            txtSurname.text = null
+            chkPolite.isChecked = false
+        }
+
+    }
+
 }
